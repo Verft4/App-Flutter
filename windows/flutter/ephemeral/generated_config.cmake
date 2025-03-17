adbd24060241f762cd2b648d5a24f5a15dc6d7ed
Rhwan_Freitas <149215970+Verft4@users.noreply.github.com>
2025-03-17 01:13:58 -0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\rhwan\\Downloads\\flutter_windows_3.24.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\rhwan\\.vscode\\PSI3\\namer_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\rhwan\\Downloads\\flutter_windows_3.24.4-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\rhwan\\.vscode\\PSI3\\namer_app"
+  "FLUTTER_ROOT=C:\\Users\\rhwan\\Downloads\\flutter_windows_3.24.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\rhwan\\.vscode\\PSI3\\namer_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\rhwan\\.vscode\\PSI3\\namer_app"
+  "FLUTTER_TARGET=C:\\Users\\rhwan\\.vscode\\PSI3\\namer_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\rhwan\\.vscode\\PSI3\\namer_app\\.dart_tool\\package_config.json"
+)
